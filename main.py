5030216ad8f7309fe8da3d1433d1615729106849
Jun00000000000000000 <okdtayjrg@gmail.com>
2021-01-03 14:03:30 +0000

    test

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,messages=TextSendMessage(text="気のせいか..."))
         state = 1
     else:
-        state = 1
+        state = 0
 
 if __name__ == "__main__":
 #    app.run()
