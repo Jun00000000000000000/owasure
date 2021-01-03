2d7cdefb145ceccdea73eb58ab45b6b76fdf8335
Jun00000000000000000 <okdtayjrg@gmail.com>
2021-01-03 15:22:05 +0000

    modify state

--- main.py
+++ main.py
@@ -55,6 +55,10 @@ def handle_get_request():
 
     return str(tmp)
 
+@app.route("/Sensor",methods=["GET"])
+def handle_get_request2():
+    line_bot_api.push_message("Ufe327b70ea9290e56a4a2e7fabd00165",TextSendMessage(text="test"))
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text=event.message.text
