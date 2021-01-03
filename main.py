0c0079e911b71f28430f9e39599f273003581fb0
Jun00000000000000000 <okdtayjrg@gmail.com>
2021-01-03 13:19:02 +0000

    show [qiita]robot arm

--- main.py
+++ main.py
@@ -88,5 +88,5 @@ def handle_message(event):
 
 if __name__ == "__main__":
 #    app.run()
-    port = int(os.getenv("PORT",8000))
+    port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
