fb2ded003b14f96bf562b16c8aea3d42d3ca8573
Jun00000000000000000 <okdtayjrg@gmail.com>
2021-01-06 00:42:05 +0900

    time module

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ def handle_get_request2(event):
             ]
         )
     )
-    line_bot_api.push_message(event.reply_token,messages=confirm_template_message)
-    #line_bot_api.push_message("Ufe327b70ea9290e56a4a2e7fabd00165",messages=confirm_template_message)
+    #line_bot_api.push_message(event.reply_token,messages=confirm_template_message)
+    line_bot_api.push_message("Ufe327b70ea9290e56a4a2e7fabd00165",messages=confirm_template_message)
     #time.sleep(20)
     #TimeCounter()
 
