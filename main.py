1c9d5a742308eab95bcb85ac0eb898b054612219
Jun00000000000000000 <okdtayjrg@gmail.com>
2021-01-06 01:30:15 +0900

    time module

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def handle_get_request():
     return str(tmp)
 
 @app.route("/Sensor",methods=["GET"])
-def handle_get_request2():
+def handle_get_request2(event):
     confirm_template_message = TemplateSendMessage(
         alt_text='Confirm template',
         template=ConfirmTemplate(
@@ -73,28 +73,11 @@ def handle_get_request2():
             ]
         )
     )
-    line_bot_api.push_message("Ufe327b70ea9290e56a4a2e7fabd00165",messages=confirm_template_message)
+    line_bot_api.push_message(event.reply_token,messages=confirm_template_message)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text=event.message.text
     global state
-    confirm_template_message = TemplateSendMessage(
-        alt_text='Confirm template',
-        template=ConfirmTemplate(
-            text='部屋の電気が付けっぱなしになっていませんか？照明のON/OFFを選択してください',
-            actions=[
-                MessageAction(
-                    label='ON',
-                    text='消しません'
-                ),
-                MessageAction(
-                    label='OFF',
-                    text='消します'
-                )
-            ]
-        )
-    )
     if text == "消します":
         line_bot_api.reply_message(event.reply_token,messages=TextSendMessage(text="部屋の電気はOFFの状態です！"))
         state = 1
