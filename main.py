43ce2d0756b04b2d739e7e69ad63ac1ec3a380b0
Jun00000000000000000 <okdtayjrg@gmail.com>
2021-01-05 13:59:25 +0000

    add TimeCounter()

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def handle_get_request2(event):
     )
     line_bot_api.push_message(event.reply_token,messages=confirm_template_message)
     #line_bot_api.push_message("Ufe327b70ea9290e56a4a2e7fabd00165",messages=confirm_template_message)
-    tkin.after(60,TimeCounter)
+    tkin.after(20,TimeCounter)
 return confirm_template_message
 
 def TimeCounter():
