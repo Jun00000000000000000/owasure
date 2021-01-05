ce9380efc50266538593eb10a2f6b08e1c1ce69f
Jun00000000000000000 <okdtayjrg@gmail.com>
2021-01-06 01:16:44 +0900

    time module

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,CarouselTemplate, CarouselColumn, ConfirmTemplate, PostbackAction, MessageAction, URIAction
     )
-import time
 
 app = Flask(__name__)
 
@@ -50,10 +49,10 @@ def handle_get_request():
 
     tmp = state
     state = 0
-    #tmpの値はstateの値が変わっても変化していないことに注意
     if tmp!=0:
         print("accept_"+str(tmp))
         #line_bot_api.push_message("Ufe327b70ea9290e56a4a2e7fabd00165",TextSendMessage(text="accept_"+str(tmp)))
+
     return str(tmp)
 
 @app.route("/Sensor",methods=["GET"])
@@ -76,11 +75,26 @@ def handle_get_request2():
     )
     line_bot_api.push_message("Ufe327b70ea9290e56a4a2e7fabd00165",messages=confirm_template_message)
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text=event.message.text
     global state
+    confirm_template_message = TemplateSendMessage(
+        alt_text='Confirm template',
+        template=ConfirmTemplate(
+            text='部屋の電気が付けっぱなしになっていませんか？照明のON/OFFを選択してください',
+            actions=[
+                MessageAction(
+                    label='ON',
+                    text='消しません'
+                ),
+                MessageAction(
+                    label='OFF',
+                    text='消します'
+                )
+            ]
+        )
+    )
     if text == "消します":
         line_bot_api.reply_message(event.reply_token,messages=TextSendMessage(text="部屋の電気はOFFの状態です！"))
         state = 1
@@ -89,6 +103,7 @@ def handle_message(event):
         state = 2
     else:
         state = 0
+
 
 if __name__ == "__main__":
 #    app.run()
