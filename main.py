61ecefad2605ee05ab0e40e3bc44941b72ef3c3b
Jun00000000000000000 <okdtayjrg@gmail.com>
2021-01-06 22:50:56 +0900

    userid changed to channelid

--- main.py
+++ main.py
@@ -55,25 +55,25 @@ def handle_get_request():
 
     return str(tmp)
 
-#@app.route("/Sensor",methods=["GET"])
-#def send_button():
-#    confirm_template_message = TemplateSendMessage(
-#        alt_text='Confirm template',
-#        template=ConfirmTemplate(
-#            text='部屋の電気が付けっぱなしになっていませんか？照明のON/OFFを選択してください',
-#            actions=[
-#                MessageAction(
-#                    label='ON',
-#                    text='消しません'
-#                ),
-#                MessageAction(
-#                    label='OFF',
-#                    text='消します'
-#                )
-#            ]
-#        )
-#    )
-#    line_bot_api.push_message("U045dd3598915d36e27c3e651b83479b2",messages=confirm_template_message)
+@app.route("/Sensor",methods=["GET"])
+def send_button():
+    confirm_template_message = TemplateSendMessage(
+        alt_text='Confirm template',
+        template=ConfirmTemplate(
+            text='部屋の電気が付けっぱなしになっていませんか？照明のON/OFFを選択してください',
+            actions=[
+                MessageAction(
+                    label='ON',
+                    text='消しません'
+                ),
+                MessageAction(
+                    label='OFF',
+                    text='消します'
+                )
+            ]
+        )
+    )
+    line_bot_api.push_message("U045dd3598915d36e27c3e651b83479b2",messages=confirm_template_message)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
